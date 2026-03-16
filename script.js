f65b4620f5b17476c8b742c7e5e069d9f90f8db8
airamabutin321 <fmabutinairao@gmail.com>
2026-03-16 02:16:15 -0700

    Add alerts for different page IDs in script.js

---
+++ script.js
@@ -0,0 +1,22 @@
+if (document.body.id === "page1") {
+    alert("👻 Welcome to Honeyvies! Ready for a scary movie?");
+}
+
+
+if (document.body.id === "page2") {
+    alert("👻 Welcome to the Movie List! Click a poster to see its rating!");
+}
+
+
+if (document.body.id === "page3") {
+    alert("🎬 Welcome to Top Picks! Check out my favorite horror movies! 👻");
+}
+
+
+function movieAlert(message) {
+  alert(message);
+};
+
+function sendMessage() {
+  alert("✅ Your message has been sent! Thank you for contacting Honeyvies.");
+};
